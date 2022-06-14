d781f4a51431754dc76dabf732f9ac9cb5689f71
Simon <simimueller@hotmail.com>
2022-06-15 00:21:51 +0200

    try out new style

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -9,25 +9,23 @@
 </svelte:head>
 
 <div class="main">
-	<div class="text">
-		<h1>Hey, I'm Simon</h1>
-		<p>I'm currently working on a new portfolio website.</p>
-		<a href="mailto:hey@seimon.ch">Get in touch</a>
-	</div>
-
-	<div class="image">
-		<img class="simon" alt="Portrait of Simon Müller" src={simon} />
-	</div>
-	<div class="links">
-		<a class="mail" href="mailto:hey@seimon.ch">
-			<img alt="The project logo" src={logo} />
-		</a>
-		<a class="linkedin" href="https://www.linkedin.com/in/seimon-m">
-			<img alt="The project logo" src={logo} />
-		</a>
-		<a class="twitter" href="https://twitter.com/seimon_m">
-			<img alt="The project logo" src={logo} />
-		</a>
+	<h1>Hey, I'm Simon</h1>
+	<div class="card">
+		<div class="image">
+			<img class="simon" alt="Portrait of Simon Müller" src={simon} />
+		</div>
+		<div class="text">
+			<p>This site is under heavy development.</p>
+			<p>For now, just get in touch:</p>
+		</div>
+		<div class="links">
+			<a class="mail" href="mailto:hey@seimon.ch">
+				<img alt="Mail Logo" src={logo} />
+			</a>
+			<a class="linkedin" href="https://www.linkedin.com/in/seimon-m">
+				<img alt="Linkedin Logo" src={logo} />
+			</a>
+		</div>
 	</div>
 </div>
 
@@ -35,10 +33,27 @@
 	.main {
 		height: 100vh;
 		width: 100vw;
-		padding: 20px;
 		display: flex;
 		flex-direction: column;
 		align-items: center;
+	}
+
+	h1 {
+		position: absolute;
+	}
+
+	.card {
+		/* From https://css.glass */
+		background: rgba(255, 255, 255, 0.2);
+		border-radius: 16px;
+		box-shadow: 0 4px 30px rgba(0, 0, 0, 0.1);
+		backdrop-filter: blur(5px);
+		-webkit-backdrop-filter: blur(5px);
+		border: 1px solid rgba(255, 255, 255, 0.3);
+		/* width: 80vw; */
+		/* height: 100vh; */
+		margin: 20px;
+		padding: 10px;
 	}
 
 	.image {
@@ -49,10 +64,11 @@
 		display: flex;
 		flex-direction: row;
 		justify-content: space-around;
+		margin: 20px;
 	}
 
 	.links > a {
-		margin: 30px;
+		margin: 0px 30px;
 	}
 
 	.links > a > img {
@@ -62,5 +78,7 @@
 
 	.simon {
 		max-width: 80vw;
+		max-height: 55vh;
+		border-radius: 16px;
 	}
 </style>
